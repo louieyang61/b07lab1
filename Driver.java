ba7923a547c3ed638fdd17db3df2f6935364b6bd
louieyang <louieyang61@gmail.com>
2025-06-01 23:56:33 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,54 @@
+import java.io.File;
+
 public class Driver {
-	public static void main(String [] args) {
-		Polynomial p = new Polynomial();
-		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
-		Polynomial s = p1.add(p2);
-		System.out.println("s(0.1) = " + s.evaluate(0.1));
-		if(s.hasRoot(1))
+	public static void main(String[] args) {
+		// Create an empty polynomial and print its evaluation at x = 3
+		Polynomial emptyPolynomial = new Polynomial();
+		System.out.println(emptyPolynomial.evaluate(3));  // Expect 0.0
+
+		// Build first polynomial: 6 + 5x^3
+		double[] coefficients1 = { 6.0, 5.0 };
+		int[] exponents1 = { 0, 3 };
+		Polynomial poly1 = new Polynomial(coefficients1, exponents1);
+
+		// Build second polynomial: -2x - 9x^4
+		double[] coefficients2 = { -2.0, -9.0 };
+		int[] exponents2 = { 1, 4 };
+		Polynomial poly2 = new Polynomial(coefficients2, exponents2);
+
+		// Add poly1 and poly2 → s(x)
+		Polynomial sumPoly = poly1.add(poly2);
+		System.out.println("s(0.1) = " + sumPoly.evaluate(0.1));
+
+		// Check if x = 1 is a root of s(x)
+		if (sumPoly.hasRoot(1.0)) {
 			System.out.println("1 is a root of s");
-		else
+		} else {
 			System.out.println("1 is not a root of s");
+		}
+
+		// Multiply poly1 by poly2 → t(x)
+		Polynomial productPoly = poly1.multiply(poly2);
+		System.out.println("t(1) = " + productPoly.evaluate(1.0));
+		System.out.println("t(2) = " + productPoly.evaluate(2.0));
+		System.out.println("t(0) = " + productPoly.evaluate(0.0));
+
+		// Save poly1 to a file "poly1.txt", then read it back into a new Polynomial
+		String outputFilename = "poly1.txt";
+		poly1.saveToFile(outputFilename);
+
+		File savedFile = new File(outputFilename);
+		Polynomial loadedPoly = new Polynomial(savedFile);
+
+		// Add the loaded polynomial to poly2 → s2(x)
+		Polynomial sumWithLoaded = loadedPoly.add(poly2);
+		System.out.println("s2(0.1) = " + sumWithLoaded.evaluate(0.1));
+
+		// Check if x = 1 is a root of s2(x)
+		if (sumWithLoaded.hasRoot(1.0)) {
+			System.out.println("1 is a root of s2");
+		} else {
+			System.out.println("1 is not a root of s2");
+		}
 	}
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,67 +1,198 @@
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
 public class Polynomial {
-    double[] coefficients;
+    double[] co;
+    int[] co_i;
 
     public Polynomial() {
-        this.coefficients = new double[1];
+        this.co = new double[1];
+        this.co_i = new int[1];
+        return;
     }
 
-    public Polynomial(double[] inputCoefficients) {
-        this.coefficients = inputCoefficients;
+    public Polynomial(double[] coe, int[] coe_i) {
+        this.co = coe;
+        this.co_i = coe_i;
+        return;
     }
 
-    public Polynomial add(Polynomial other) {
-        int thisDegree = this.coefficients.length;
-        int otherDegree = other.coefficients.length;
-
-        if (thisDegree < otherDegree) {
-            double[] newCoefficients = new double[otherDegree];
-
-            for (int i = 0; i < thisDegree; i++) {
-                newCoefficients[i] = other.coefficients[i] + this.coefficients[i];
+    public Polynomial(File f){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String pol = scanner.nextLine();
+        String modPol = pol.replaceAll("-", "\\+-");
+        String[] terms = modPol.split("\\+");
+        int termNum = terms.length;
+        double[] coe = new double[termNum];
+        int[] fr = new int[termNum];
+        for (int i = 0; i < termNum; ++i) {
+            String ter = terms[i];
+            if(ter.contains("x")){
+                String[] cf = ter.split("x");
+                if (cf.length == 2){
+                    coe[i] = Double.parseDouble(cf[0]);
+                    fr[i] = Integer.parseInt(cf[1]);
+                }
+                else{
+                    coe[i] = Double.parseDouble(cf[0]);
+                    fr[i] = 1;
+                }
             }
-
-            for (int i = thisDegree; i < otherDegree; i++) {
-                newCoefficients[i] = other.coefficients[i];
+            else{
+                coe[i] = Double.parseDouble(ter);
+                fr[i] = 0;
             }
-
-            return new Polynomial(newCoefficients);
         }
+        this.co = coe;
+        this.co_i = fr;
+        return;
+    }
 
-        if (thisDegree == otherDegree) {
-            double[] newCoefficients = new double[thisDegree];
-
-            for (int i = 0; i < thisDegree; i++) {
-                newCoefficients[i] = other.coefficients[i] + this.coefficients[i];
+    public void saveToFile(String fn){
+        File file = new File(fn);
+        try {
+            if (file.createNewFile()){
+                String pol ="";
+                for(int i = 0; i < this.co.length - 1; ++i){
+                    if(co_i[i] == 0){
+                        pol = pol + co[i] + "+";
+                    }
+                    else if (co_i[i] == 1){
+                        pol = pol + co[i] + "x" + "+";
+                    }
+                    else{
+                        pol = pol + co[i] + "x" + co_i[i] + "+";
+                    }
+                }
+                pol = pol + co[this.co.length - 1] + "x" + co_i[this.co_i.length - 1];
+                pol = pol.replaceAll("\\+-", "-");
+                FileWriter fwr = new FileWriter(file);
+                BufferedWriter bwr = new BufferedWriter(fwr);
+                bwr.write(pol);
+                bwr.close();
+                return;
             }
+            else{
+                String pol ="";
+                for(int i = 0; i < this.co.length - 1; ++i){
+                    if(co_i[i] == 0){
+                        pol = pol + co[i] + "+";
+                    }
+                    else if (co_i[i] == 1){
+                        pol = pol + co[i] + "x" + "+";
+                    }
+                    else{
+                        pol = pol + co[i] + "x" + co_i[i] + "+";
+                    }
+                }
+                pol = pol + co[this.co.length - 1] + "x" + co_i[this.co_i.length - 1];
+                pol = pol.replaceAll("\\+-", "-");
+                FileWriter fwr = new FileWriter(file);
+                BufferedWriter bwr = new BufferedWriter(fwr);
+                bwr.write(pol);
+                bwr.close();
+                return;
+            }
+        } catch(IOException e){
+            System.out.println(" Something Wrong Happend");
+            e.printStackTrace();
+        }
+        return;
+    }
 
-            return new Polynomial(newCoefficients);
+    public Polynomial add(Polynomial a) {
+        int thisLen = this.co_i[this.co_i.length - 1];
+        int aLen = a.co_i[a.co_i.length - 1];
+        int newLen = Math.max(thisLen, aLen) + 1;
+
+        double[] add_co = new double[newLen];
+
+        for (int i = 0; i < this.co_i.length; ++i){
+            add_co[this.co_i[i]] = this.co[i];
         }
 
-        double[] newCoefficients = new double[thisDegree];
-
-        for (int i = 0; i < otherDegree; i++) {
-            newCoefficients[i] = other.coefficients[i] + this.coefficients[i];
+        for (int j = 0; j < a.co_i.length; ++j){
+            add_co[a.co_i[j]] = add_co[a.co_i[j]] + a.co[j];
         }
 
-        for (int i = otherDegree; i < thisDegree; i++) {
-            newCoefficients[i] = this.coefficients[i];
+        int no_zero = 0;
+        for (int p = 0; p < newLen; ++p){
+            if (add_co[p] != 0){
+                no_zero += 1;
+            }
         }
 
-        return new Polynomial(newCoefficients);
+        double[] true_add_co = new double[no_zero];
+        int[] true_add_co_i = new int[no_zero];
+        int t = 0;
+        for(int k = 0; k < newLen; ++k){
+            if(add_co[k] != 0){
+                true_add_co[t] = add_co[k];
+                true_add_co_i[t] = k;
+                t += 1;
+            }
+        }
+
+        Polynomial newp = new Polynomial(true_add_co, true_add_co_i);
+        return newp;
     }
 
     public double evaluate(double x) {
-        int degree = this.coefficients.length;
-        double result = 0.0;
+        int len = this.co.length;
+        double result = 0.0d;
 
-        for (int i = 0; i < degree; i++) {
-            result += this.coefficients[i] * Math.pow(x, i);
+        for (int i = 0; i < len; ++i){
+            result = result + this.co[i] * (Math.pow(x, this.co_i[i]));
         }
 
         return result;
     }
 
     public boolean hasRoot(double x) {
-        return evaluate(x) == 0.0;
+        double e = evaluate(x);
+        return e == 0.0d;
+    }
+
+    public Polynomial multiply(Polynomial jyf) {
+        int thisLen = this.co_i[this.co_i.length - 1];
+        int jLen = jyf.co_i[jyf.co_i.length - 1];
+        int newLen = thisLen + jLen + 1;
+        double[] new_mul_co = new double[newLen];
+
+        for (int i = 0; i < this.co.length; ++i){
+            double ic = this.co[i];
+            for (int j = 0; j < jyf.co.length; ++j) {
+                new_mul_co[this.co_i[i] + jyf.co_i[j]] = new_mul_co[this.co_i[i] + jyf.co_i[j]] + ic * jyf.co[j];
+            }
+        }
+
+        int no_zero = 0;
+        for (int p = 0; p < newLen; ++p){
+            if (new_mul_co[p] != 0){
+                no_zero += 1;
+            }
+        }
+
+        double[] true_mul_co = new double[no_zero];
+        int[] true_mul_co_i = new int[no_zero];
+        int t = 0;
+        for(int k = 0; k < newLen; ++k){
+            if(new_mul_co[k] != 0){
+                true_mul_co[t] = new_mul_co[k];
+                true_mul_co_i[t] = k;
+                t += 1;
+            }
+        }
+        Polynomial nmp = new Polynomial(true_mul_co, true_mul_co_i);
+        return nmp;
     }
 }
